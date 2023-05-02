bb9ac60ffb007f0e6a3108169e9d43bb98cbbeec
Matt Wise <wsmatth@amazon.com>
2023-05-02 10:46:53 -0500

    Upgrade to Release 4.2.1

--- source/machine_connector/utils/__init__.py
+++ source/machine_connector/utils/__init__.py
@@ -6,4 +6,4 @@ from .pickle_checkpoint_manager import PickleCheckpointManager
 from .stream_manager_helper import StreamManagerHelperClient
 from .init_msg_metadata import InitMessage
 
-__version__ = "4.2.0"
+__version__ = "4.2.1"
